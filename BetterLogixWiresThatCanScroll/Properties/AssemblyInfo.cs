12fac683cd5d0a3b09ba030c16ad79f0ed5ea2ae
Arne Kiesewetter <banane9@live.de>
2022-04-17 21:44:25 +0200

    Update AssemblyInfo

--- BetterLogixWiresThatCanScroll/Properties/AssemblyInfo.cs
+++ BetterLogixWiresThatCanScroll/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BetterLogixWiresThatScroll")]
+[assembly: AssemblyTitle("BetterLogixWiresThatCanScroll")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BetterLogixWiresThatScroll")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCompany("eia485|Banane9")]
+[assembly: AssemblyProduct("BetterLogixWiresThatCanScroll")]
+[assembly: AssemblyCopyright("Copyright © eia485|Banane9 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
