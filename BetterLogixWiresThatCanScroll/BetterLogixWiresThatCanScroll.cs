d0ca2c5d1004b1e432bced367fd96a684b86f045
Arne Kiesewetter <banane9@live.de>
2022-04-17 21:43:39 +0200

    Implement configurable scrolling and refactor

---
+++ BetterLogixWiresThatCanScroll/BetterLogixWiresThatCanScroll.cs
@@ -0,0 +1,129 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using System;
+using FrooxEngine;
+using FrooxEngine.LogiX;
+using BaseX;
+
+namespace BetterLogixWiresThatCanScroll
+{
+    public class BetterLogixWiresThatCanScroll : NeosMod
+    {
+        [AutoRegisterConfigKey]
+        public static ModConfigurationKey<bool> AnimateWires = new ModConfigurationKey<bool>("AnimateWires", "Make the arrows on LogiX wires scroll.", () => false);
+
+        public static ModConfiguration Config;
+        public override string Author => "eia485|Banane9";
+        public override string Link => "https://github.com/Banane9/NeosBetterLogixWiresThatCanScroll";
+        public override string Name => "BetterLogixWiresThatCanScroll";
+        public override string Version => "1.1.0";
+
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony($"{Author}.{Name}");
+            Config = GetConfiguration();
+            Config.Save(true);
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(ConnectionWire))]
+        private static class BetterLogixWiresThatScrollPatch
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch("DeleteHighlight")]
+            private static bool DeleteHighlightPrefix(SyncRef<FresnelMaterial> ___Material, SyncRef<Slot> ___WireSlot, ConnectionWire __instance)
+            {
+                Type wireType = __instance.InputField.Target.GetType().GetWireType();
+
+                ___Material.Target = __instance.GetWireMaterial(color.Red, wireType.GetDimensions(), wireType.IsImpulse());
+                ___WireSlot.Target.GetComponent<MeshRenderer>().Materials[0] = ___Material.Target;
+
+                return false;
+            }
+
+            [HarmonyPrefix]
+            [HarmonyPatch("OnAttach")]
+            private static bool OnAttachPrefix(SyncRef<Slot> ___WireSlot, FieldDrive<float3> ___WirePoint, FieldDrive<float3> ___WireTangent, FieldDrive<floatQ> ___WireOrientation, FieldDrive<float> ___WireWidth, ConnectionWire __instance)
+            {
+                ___WireSlot.Target = __instance.Slot.AddSlot("Wire");
+
+                StripeWireMesh stripeWireMesh = ___WireSlot.Target.AttachComponent<StripeWireMesh>();
+                stripeWireMesh.Orientation0.Value = floatQ.Euler(0f, 0f, -90f);
+
+                SyncField<float3> tangent = stripeWireMesh.Tangent0;
+                float3 left = float3.Left;
+                tangent.Value = (left) * 0.25f;
+
+                stripeWireMesh.Width0.Value = 0.025600001f;
+                ___WirePoint.Target = stripeWireMesh.Point1;
+                ___WireTangent.Target = stripeWireMesh.Tangent1;
+                ___WireOrientation.Target = stripeWireMesh.Orientation1;
+                ___WireWidth.Target = stripeWireMesh.Width1;
+
+                MeshCollider meshCollider = ___WireSlot.Target.AttachComponent<MeshCollider>();
+                meshCollider.Mesh.Target = stripeWireMesh;
+                meshCollider.Sidedness.Value = MeshColliderSidedness.DualSided;
+
+                ___WireSlot.Target.AttachComponent<SearchBlock>();
+                ___WireSlot.Target.ActiveSelf = false;
+                ___WireSlot.Target.AttachComponent<MeshRenderer>().Mesh.Target = stripeWireMesh;
+
+                return false;
+            }
+
+            [HarmonyPrefix]
+            [HarmonyPatch("SetTypeColor")]
+            private static bool SetTypeColorPrefix(SyncRef<FresnelMaterial> ___Material, Sync<color> ___TypeColor, SyncRef<Slot> ___WireSlot, ConnectionWire __instance)
+            {
+                Type wireType = __instance.InputField.Target.GetType().GetWireType();
+
+                ___Material.Target = __instance.GetWireMaterial(___TypeColor, wireType.GetDimensions(), wireType.IsImpulse());
+                ___WireSlot.Target.GetComponent<MeshRenderer>().Materials[0] = ___Material.Target;
+
+                return false;
+            }
+
+            [HarmonyPrefix]
+            [HarmonyPatch("SetupStyle")]
+            private static bool SetupStylePrefix(color color, int dimensions, bool isImpulse, Sync<color> ___TypeColor, SyncRef<FresnelMaterial> ___Material, SyncRef<Slot> ___WireSlot, ConnectionWire __instance)
+            {
+                ___TypeColor.Value = color;
+                ___Material.Target = __instance.GetWireMaterial(color, dimensions, isImpulse);
+                ___WireSlot.Target.GetComponent<MeshRenderer>().Materials.Add(___Material.Target);
+
+                return false;
+            }
+
+            [HarmonyPrefix]
+            [HarmonyPatch("SetupTempWire")]
+            private static bool SetupTempWirePrefix(Slot targetPoint, bool output, Sync<bool> ___TempWire, SyncRef<FresnelMaterial> ___Material, SyncRef<Slot> ___WireSlot, Sync<color> ___TypeColor, ConnectionWire __instance)
+            {
+                ___TempWire.Value = true;
+                __instance.TargetSlot.Target = targetPoint;
+                ___WireSlot.Target.ActiveSelf = true;
+
+                ___Material.Target = ___WireSlot.Target.DuplicateComponent(___Material.Target);
+
+                var value = new float2(0, 1);
+                ___Material.Target.FarTextureScale.Value = value;
+                ___Material.Target.NearTextureScale.Value = value;
+
+                ___WireSlot.Target.GetComponent<MeshRenderer>().Materials[0] = ___Material.Target;
+
+                __instance.SetupAsOutput(output);
+
+                foreach (var collider in ___WireSlot.Target.GetComponents<MeshCollider>())
+                    collider.Destroy();
+
+                if (!Config.GetValue(AnimateWires))
+                    return false;
+
+                Panner2D panner = ___WireSlot.Target.AttachComponent<Panner2D>();
+                panner.Speed = new float2(___TypeColor == color.White ? -1 : 1, 0);
+                panner.Target = ___Material.Target.NearTextureOffset;
+
+                return false;
+            }
+        }
+    }
+}
---
+++ BetterLogixWiresThatCanScroll/Helpers.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BaseX;
+using FrooxEngine;
+using FrooxEngine.LogiX;
+
+namespace BetterLogixWiresThatCanScroll
+{
+    internal static class Helpers
+    {
+        private static readonly Type impulseType = typeof(Impulse);
+
+        private static bool animateWires => BetterLogixWiresThatCanScroll.Config.GetValue(BetterLogixWiresThatCanScroll.AnimateWires);
+
+        public static void CleanList(this ISyncList list)
+        {
+            for (int i = list.Count - 1; i >= 0; --i)
+            {
+                ISyncMember syncMember = list.GetElement(i);
+
+                if ((syncMember as FieldDrive<float2>)?.Target == null)
+                {
+                    list.RemoveElement(i);
+                }
+            }
+        }
+
+        public static FresnelMaterial GetWireMaterial(this ConnectionWire instance, color color, int dimensions, bool isImpulse)
+        {
+            Slot LogixAssets = instance.World.AssetsSlot.FindOrAdd("LogixAssets");
+
+            // Use extended key for static material to be backwards compatible with old version
+            var key = $"Logix_WireMaterial_{color}_{(isImpulse ? "Impulse" : "Value")}_{dimensions}{(animateWires ? "" : "_Static")}";
+
+            var fresnelMaterial = (FresnelMaterial)instance.World.KeyOwner(key);
+            if (fresnelMaterial == null)
+            {
+                fresnelMaterial = LogixAssets.AttachComponent<FresnelMaterial>();
+                fresnelMaterial.AssignKey(key, 2);
+                fresnelMaterial.BlendMode.Value = BlendMode.Alpha;
+                fresnelMaterial.ZWrite.Value = ZWrite.On;
+                fresnelMaterial.Sidedness.Value = Sidedness.Double;
+
+                var wireTexture = LogixHelper.GetWireTexture(instance.World, dimensions, isImpulse);
+                wireTexture.WrapModeU.Value = TextureWrapMode.Repeat;
+
+                fresnelMaterial.NearTexture.Target = wireTexture;
+                fresnelMaterial.FarTexture.Target = wireTexture;
+
+                var value = new float2(0.5f, 1f);
+                fresnelMaterial.NearTextureScale.Value = value;
+                fresnelMaterial.FarTextureScale.Value = value;
+
+                fresnelMaterial.NearColor.Value = color.MulA(.8f);
+                fresnelMaterial.FarColor.Value = color.MulRGB(.5f).MulA(.8f);
+            }
+
+            if (!animateWires)
+                return fresnelMaterial;
+
+            ValueMultiDriver<float2> multiDriver;
+            var pannerKey = $"Logix_WirePanner_{(isImpulse ? "Impulse" : "Value")}";
+
+            var panner = (Panner2D)instance.World.KeyOwner(pannerKey);
+            if (panner == null)
+            {
+                panner = LogixAssets.AttachComponent<Panner2D>();
+                panner.Speed = new float2(isImpulse ? -1 : 1, 0);
+                panner.AssignKey(pannerKey, 2);
+
+                multiDriver = LogixAssets.AttachComponent<ValueMultiDriver<float2>>();
+                panner.Target = multiDriver.Value;
+            }
+            else
+            {
+                multiDriver = panner?.Target?.Parent as ValueMultiDriver<float2>;
+                if (multiDriver == null)
+                {
+                    multiDriver = LogixAssets.AttachComponent<ValueMultiDriver<float2>>();
+                    panner.Target = multiDriver.Value;
+                }
+            }
+
+            ISyncList listOfDrives = multiDriver.Drives;
+            listOfDrives.CleanList();
+
+            if (fresnelMaterial.NearTextureOffset.IsDriven || fresnelMaterial.NearTextureOffset.IsLinked)
+            {
+                if ((fresnelMaterial.NearTextureOffset.ActiveLink as SyncElement)?.Component != multiDriver)
+                {
+                    ((FieldDrive<float2>)listOfDrives.AddElement()).ForceLink(fresnelMaterial.NearTextureOffset);
+                }
+            }
+            else
+                ((FieldDrive<float2>)listOfDrives.AddElement()).Target = fresnelMaterial.NearTextureOffset;
+
+            return fresnelMaterial;
+        }
+
+        public static Type GetWireType(this Type inputType)
+        {
+            return inputType.IsGenericType ? inputType.GetGenericArguments()[0] : inputType;
+        }
+
+        public static bool IsImpulse(this Type wireType)
+        {
+            return wireType == impulseType;
+        }
+    }
+}
