0ef9b00529d8a1fc55f0099dcdc4dce16b8c1e28
Arne Kiesewetter <banane9@live.de>
2022-04-25 16:42:32 +0200

    Properly fix connection wire sides.

--- BetterLogixWiresThatCanScroll/BetterLogixWiresThatCanScroll.cs
+++ BetterLogixWiresThatCanScroll/BetterLogixWiresThatCanScroll.cs
@@ -5,6 +5,8 @@ using FrooxEngine;
 using FrooxEngine.LogiX;
 using BaseX;
 using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace BetterLogixWiresThatCanScroll
 {
@@ -14,10 +16,11 @@ namespace BetterLogixWiresThatCanScroll
         public static ModConfigurationKey<bool> AnimateWires = new ModConfigurationKey<bool>("AnimateWires", "Make the arrows on LogiX wires scroll.", () => false);
 
         public static ModConfiguration Config;
+
         public override string Author => "Banane9";
         public override string Link => "https://github.com/Banane9/NeosBetterLogixWiresThatCanScroll";
         public override string Name => "BetterLogixWiresThatCanScroll";
-        public override string Version => "1.1.1";
+        public override string Version => "1.2.0";
 
         public override void OnEngineInit()
         {
@@ -30,8 +33,6 @@ namespace BetterLogixWiresThatCanScroll
         [HarmonyPatch(typeof(ConnectionWire))]
         private static class BetterLogixWiresThatScrollPatch
         {
-            private static readonly Type outputAttribute = typeof(AsOutput);
-
             [HarmonyPrefix]
             [HarmonyPatch("DeleteHighlight")]
             private static bool DeleteHighlightPrefix(SyncRef<FresnelMaterial> ___Material, SyncRef<Slot> ___WireSlot, ConnectionWire __instance)
@@ -114,13 +115,7 @@ namespace BetterLogixWiresThatCanScroll
                 ___WireSlot.Target.GetComponent<MeshRenderer>().Materials[0] = ___Material.Target;
                 ___WireSlot.Target.GetComponent<MeshCollider>()?.Destroy();
 
-                var input = __instance.Slot.GetComponentInParents<InputProxy>()?.InputField.Target;
-                var inputParent = input?.FindNearestParent<LogixNode>();
-
-                if (output || (input != null && Enumerable.Range(0, inputParent.SyncMemberCount)
-                                        .Select(i => inputParent.GetSyncMemberFieldInfo(i))
-                                        .Where(field => field.GetCustomAttributes(outputAttribute, false).Any())
-                                        .Any(field => field.GetValue(inputParent) == input)))
+                if (output)
                     __instance.SetupAsOutput();
 
                 if (Config.GetValue(AnimateWires))
@@ -131,6 +126,189 @@ namespace BetterLogixWiresThatCanScroll
                 }
 
                 return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(LogixTip))]
+        private static class LogixTipPatch
+        {
+            [HarmonyPatch("CheckProxyCandidate")]
+            [HarmonyReversePatch(HarmonyReversePatchType.Snapshot)]
+            private static void CheckProxyCandidate(LogixTip @this, Component proxy, float3 origin, ref Component closestProxy, ref float closestDistance)
+            {
+                throw new NotImplementedException("It's a reverse patch.");
+            }
+
+            [HarmonyPrefix]
+            [HarmonyPatch(nameof(LogixTip.OnPrimaryPress))]
+            private static bool OnPrimaryPressPrefix(LogixTip __instance, ref Action ____impulseTarget, ref Impulse ____impulseSource, ref IDriverNode ____driver, ref IWorldElement ____output, ref IInputElement ____input, ref double ___lastPrimaryPress, ref float3 ___deletePoint, ref SegmentMesh ____deleteLine, ref float? ___deleteDistance)
+            {
+                var traverse = Traverse.Create(__instance);
+
+                var flag = false;
+                var list = (IEnumerable)traverse.Method("GetColliderHits").GetValue();
+
+                Component closestProxy = null;
+                float closestDistance = float.MaxValue;
+
+                foreach (var item in list)
+                {
+                    var itemTraverse = Traverse.Create(item);
+                    var slot = (Slot)itemTraverse.Field("collider").Property("Slot").GetValue();
+                    var origin = (float3)itemTraverse.Field("origin").GetValue();
+
+                    InputProxy component = slot.GetComponent<InputProxy>();
+                    if (component != null)
+                    {
+                        CheckProxyCandidate(__instance, component, origin, ref closestProxy, ref closestDistance);
+                    }
+
+                    OutputProxy component2 = slot.GetComponent<OutputProxy>();
+                    if (component2 != null)
+                    {
+                        CheckProxyCandidate(__instance, component2, origin, ref closestProxy, ref closestDistance);
+                    }
+
+                    MemberProxy component3 = slot.GetComponent<MemberProxy>();
+                    if (component3 != null)
+                    {
+                        CheckProxyCandidate(__instance, component3, origin, ref closestProxy, ref closestDistance);
+                    }
+
+                    DriveProxy component4 = slot.GetComponent<DriveProxy>();
+                    if (component4 != null)
+                    {
+                        CheckProxyCandidate(__instance, component4, origin, ref closestProxy, ref closestDistance);
+                    }
+
+                    ImpulseSourceProxy component5 = slot.GetComponent<ImpulseSourceProxy>();
+                    if (component5 != null)
+                    {
+                        CheckProxyCandidate(__instance, component5, origin, ref closestProxy, ref closestDistance);
+                    }
+
+                    ImpulseTargetProxy component6 = slot.GetComponent<ImpulseTargetProxy>();
+                    if (component6 != null)
+                    {
+                        CheckProxyCandidate(__instance, component6, origin, ref closestProxy, ref closestDistance);
+                    }
+                }
+
+                Traverse.Create(typeof(Pool)).Method("Return", new object[] { list });
+
+                Type type = null;
+                if (!(closestProxy is InputProxy inputProxy))
+                {
+                    if (!(closestProxy is OutputProxy outputProxy))
+                    {
+                        if (!(closestProxy is MemberProxy memberProxy))
+                        {
+                            if (!(closestProxy is DriveProxy driveProxy))
+                            {
+                                if (!(closestProxy is ImpulseSourceProxy impulseSourceProxy))
+                                {
+                                    if (closestProxy is ImpulseTargetProxy impulseTargetProxy)
+                                    {
+                                        ____impulseTarget = impulseTargetProxy.ImpulseTarget.Target;
+                                        Slot connectionPoint = LogixHelper.GetConnectionPoint((IWorldElement)____impulseTarget.Target, ____impulseTarget.Method.Name);
+                                        SetupTempWire(__instance, connectionPoint, typeof(Action));
+                                        flag = true;
+                                    }
+                                }
+                                else
+                                {
+                                    ____impulseSource = impulseSourceProxy.ImpulseSource.Target;
+                                    Slot connectionPoint = LogixHelper.GetConnectionPoint(____impulseSource);
+                                    SetupTempWire(__instance, connectionPoint, typeof(Action), true);
+                                    flag = true;
+                                }
+                            }
+                            else
+                            {
+                                ____driver = driveProxy.Drive.Target.FindNearestParent<IDriverNode>();
+                                Slot connectionPoint = LogixHelper.GetConnectionPoint(____driver);
+                                SetupTempWire(__instance, connectionPoint, ____driver.DriveType, output: true);
+                                flag = true;
+                            }
+                        }
+                        else
+                        {
+                            ____output = memberProxy.Member.Target;
+                            IWorldElement output = ____output;
+                            type = ((output is ISyncRef syncRef) ? syncRef.TargetType : ((!(output is IField field)) ? ____output.GetType() : field.ValueType));
+                            if (____output != null)
+                            {
+                                Slot connectionPoint = LogixHelper.GetConnectionPoint(____output);
+                                SetupTempWire(__instance, connectionPoint, type, output: true);
+                                flag = true;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        var output = outputProxy.OutputField.Target;
+                        ____output = output;
+                        type = outputProxy.OutputField.Target.OutputType;
+
+                        if (____output != null)
+                        {
+                            Slot connectionPoint = LogixHelper.GetConnectionPoint(____output);
+                            SetupTempWire(__instance, connectionPoint, type, LogixHelper.GetSide(output.OwnerNode, output) == ConnectPointSide.Output);
+                            flag = true;
+                        }
+                    }
+                }
+                else
+                {
+                    ____input = inputProxy.InputField.Target;
+                    Slot connectionPoint = LogixHelper.GetConnectionPoint(____input);
+                    SetupTempWire(__instance, connectionPoint, ____input.InputType, LogixHelper.GetSide(____input.OwnerNode, ____input) == ConnectPointSide.Output);
+                    flag = true;
+                }
+
+                var activeTool = __instance.ActiveTool;
+                if (activeTool != null && (activeTool.Laser.IsTouching || activeTool.IsHoldingObjectsWithLaser))
+                    return false;
+
+                if (__instance.Time.WorldTime - ___lastPrimaryPress < 0.25)
+                {
+                    ___lastPrimaryPress = -1.0;
+                    SpawnNode(__instance);
+                    return false;
+                }
+
+                ___lastPrimaryPress = __instance.Time.WorldTime;
+
+                if (!flag)
+                {
+                    ___deletePoint = (float3)traverse.Property("DeletePointReference").GetValue();
+
+                    var attachedModel = __instance.Slot.AddSlot("DeleteLine").AttachMesh<SegmentMesh, UnlitMaterial>();
+                    attachedModel.material.BlendMode.Value = BlendMode.Alpha;
+                    attachedModel.material.TintColor.Value = new color(1f, 0f, 0f, 0.5f);
+                    attachedModel.material.ZWrite.Value = ZWrite.On;
+                    attachedModel.mesh.Radius.Value = 0.002f;
+                    attachedModel.mesh.HighPriorityIntegration.Value = true;
+
+                    ____deleteLine = attachedModel.mesh;
+                    ___deleteDistance = null;
+                }
+
+                return false;
+            }
+
+            [HarmonyPatch("SetupTempWire")]
+            [HarmonyReversePatch(HarmonyReversePatchType.Snapshot)]
+            private static void SetupTempWire(LogixTip @this, Slot connectionPoint, Type type, bool output = false)
+            {
+                throw new NotImplementedException("It's a reverse patch.");
+            }
+
+            [HarmonyPatch("SpawnNode")]
+            [HarmonyReversePatch(HarmonyReversePatchType.Snapshot)]
+            private static void SpawnNode(LogixTip @this)
+            {
+                throw new NotImplementedException("It's a reverse patch.");
             }
         }
     }
